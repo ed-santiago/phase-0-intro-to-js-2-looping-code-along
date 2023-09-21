8ff6fc652d3695fb6383cf113e74cc31266cae87
Edmond <edmond.santiago7@gmail.com>
2023-09-21 21:48:33 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+  const toWhom = [];
+  for (let i = 0; i < name.length; i++) {
+    toWhom.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return toWhom;
+}
+
+function countDown(n) {
+  while (n >= 0) {
+    console.log(n);
+    n--;
+  }
+}
+
+countDown(10);
